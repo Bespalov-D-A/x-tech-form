a69094983a9e66e9b98ebd953badd6836b47ea82
Bespalov Denis Alexandrovich <googlonator@gmail.com>
2022-12-20 01:07:53 +0400

    Create process find block

---
+++ src/components/ProcessFind/ProcessFind.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import s from "./ProcessFind.module.scss";
+import arrow from "./../../img/ico/common/Arrow.svg";
+
+const data = [
+  { msg: "Мы знакомимся с вашим проектом и его целями" },
+  { msg: "Подбираем подходящую для вашего проекта команду" },
+  { msg: "Проводим с командой мозговой штурм и включаемся в проект" },
+  { msg: "Сопровождаем команду на протяжении всего проекта" },
+];
+
+const ProcessFind: FC = () => {
+  return (
+    <div className={s["process-find"]}>
+      <h1 className={s.title}>Процесс подбора</h1>
+      <div className={s.list}>
+        {data.map((item, index) => (
+          <div key={index} className={s.item}>
+            <img src={arrow} alt="" className={s.arrow} />
+            <h1 className={s.num}>{index}</h1>
+            <p className={s.desc}>{item.msg}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProcessFind;
