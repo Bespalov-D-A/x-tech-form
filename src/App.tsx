141ad94dc634993d4d27cbce19ef5899c9ea8f68
Bespalov Denis Alexandrovich <googlonator@gmail.com>
2022-12-20 13:28:33 +0400

    Create wrap feed back

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Community from "./components/Community/Community";
 import CreateTeam from "./components/CreateTeam/CreateTeam";
 import EventComp from "./components/Event/Event";
+import Feedback from "./components/Feedback/Feedback";
 import Header from "./components/Header/Header";
 import MainMenu from "./components/MainMenu/MainMenu";
 import ProcessFind from "./components/ProcessFind/ProcessFind";
@@ -22,6 +23,7 @@ function App() {
       <ProcessFind />
       <Community />
       <EventComp />
+      <Feedback />
     </div>
   );
 }
