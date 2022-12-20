c6b3846ff70b507db05cc933d8beea6cb9050aa5
Bespalov Denis Alexandrovich <googlonator@gmail.com>
2022-12-20 13:53:31 +0400

    Create base wrap and info block in feedback

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -9,11 +9,16 @@ const Feedback: FC = () => {
         <div className={s.feedback}>
           <div className={s.info}>
             <h1 className={s.title}>свяжитесь с нами</h1>
-            <h3 className={s['where-title']}>МЫ НАХОДИМСЯ</h3>
-            <p className={s.address}>236010, РФ, г. Калининград, ул. Генерала озерова 17Б</p>
+            <h3 className={s["where-title"]}>МЫ НАХОДИМСЯ</h3>
+            <p className={s.address}>
+              236010, РФ, г. Калининград, ул. Генерала озерова 17Б
+            </p>
             <p className={s.phone}>+796543210</p>
             <p className={s.mail}>office@stl.im</p>
-            <Social />
+            <div className={s.social}>
+                <h3 className={s['social__title']}>social</h3>
+              <Social />
+            </div>
           </div>
         </div>
       </div>
