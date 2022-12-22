35f51b34a9ad657b6a80ab37563c95fb75ef283a
Bespalov Denis Alexandrovich <googlonator@gmail.com>
2022-12-22 18:30:54 +0400

    Fix X in process find

--- src/components/WhereFind/WhereFind.tsx
+++ src/components/WhereFind/WhereFind.tsx
@@ -2,20 +2,33 @@ import { FC } from "react";
 import Btn from "../common/Btn/Btn";
 import photo from "./../../img/photo/mainPage/where.jpg";
 import s from "./WhereFind.module.scss";
+import X from "../common/svg/X/X";
 
 const WhereFind: FC = () => {
   return (
-    <div className={s.wrap}>
-      <div className={s["where-find"]}>
-        <img src={photo} alt="" className={s.photo} />
-        <div className={s.desc}>
-          <h1 className={s.title}>Где мы ищем?</h1>
-          <p className={s["desc__text"]}>
-            Наша команда всегда готова к работе над вашим проектом. Хантим
-            специалистов на встречах, мероприятиях, тематических форумах,
-            boolean search.
-          </p>
-          <Btn title="Обсудить проект" pRight={40} pLeft={40} primary={true} />
+    <div className={s["wrap-line"]}>
+      <div className={s.wrap}>
+        <div className={s["where-find"]}>
+          <img src={photo} alt="" className={s.photo} />
+          <div className={s["desc-wrap"]}>
+            <div className={s.desc}>
+              <h1 className={s.title}>Где мы ищем?</h1>
+              <p className={s["desc__text"]}>
+                Наша команда всегда готова к работе над вашим проектом. Хантим
+                специалистов на встречах, мероприятиях, тематических форумах,
+                boolean search.
+              </p>
+              <Btn
+                title="Обсудить проект"
+                pRight={40}
+                pLeft={40}
+                primary={true}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={s.x}>
+          <X />
         </div>
       </div>
     </div>
---
+++ src/components/common/svg/X/X.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+
+interface XI {}
+
+const X: FC<XI> = (props) => {
+	return (
+		<svg
+			//width="429"
+			//height="400"
+			viewBox="0 0 500 400"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M0 0H78.5426L223.784 143.182H147.925L0 0ZM207.03 200L0 400H78.5426L244 237.037L409.457 400H488L280.97 200L488 0H409.457L245.168 161.813L207.03 200Z"
+				//fillgt="#F8F9FA"
+			/>
+		</svg>
+	);
+};
+
+export default X;
